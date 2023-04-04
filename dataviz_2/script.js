e7f628ca0f02ecf8584137fc18326a493b9a894c
guillebaci <guillerminabacigalupo@hotmail.com>
2023-04-03 21:23:55 -0300

    agrego las visualizaciones 2 y 3 completas

--- dataviz_2/script.js
+++ dataviz_2/script.js
@@ -1,25 +1,65 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-    let chart = Plot.plot({
-      marks: [
-        Plot.areaY(
-          data.filter(d => d.nacionalidad == 'Alemania'),
-          {
-            x: 'anio_mision',
-            y: 'mision_hs',
-            opacity: 0.5,
-            curve: 'natural',
-          },
-        ),
-      ],
-      line: true,
-      x: {
-        tickFormat: 'd',
-        ticks: 11,
-      },
-      y: {
-        ticks: 7,
-        grid: true,
-      },
-    })
-    d3.select('#chart').append(() => chart)
+  let chart = Plot.plot({
+    marks: [
+      Plot.areaY(
+        data.filter(d => d.nacionalidad == "EE.UU."),
+      Plot.groupX({ y: "sum" }, { x: "anio_mision", y: "mision_hs" , curve: 'natural', opacity: 0.7, fill:'#884EA0'}),
+      )
+    ],
+    style: {
+      backgroundColor: "#FBFCFC",
+      color: "#2E4053",
+      fontFamily: "system-ui",
+      fontSize: "14px",
+      overflow: "visible"
+    },
+    //line: true,
+    //padding:10, 
+    margin:50, 
+    height: 400,
+    width: 550,
+    x: {
+      tickFormat: 'd',
+      ticks: 9,
+    },
+    y: {
+      ticks: 7,
+      grid: true,
+      domain:[0,36000]
+    },
   })
+  d3.select('#chart1').append(() => chart)
+})
+
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+  let chart = Plot.plot({
+    marks: [
+      Plot.areaY(
+        data.filter(d => d.nacionalidad == "U.S.S.R/Rusia"),
+      Plot.groupX({ y: "sum" }, { x: "anio_mision", y: "mision_hs" , curve: 'natural', opacity: 0.7, fill:'#2980B9'}),
+      )
+    ],
+    style: {
+      backgroundColor: "#FBFCFC",
+      color: "#2E4053",
+      fontFamily: "system-ui",
+      fontSize: "14px",
+      overflow: "visible"
+    },
+    //line: true,
+    //padding:10, 
+    margin:50, 
+    height: 400,
+    width: 550,
+    x: {
+      tickFormat: 'd',
+      ticks: 9,
+    },
+    y: {
+      ticks: 7,
+      grid: true,
+      domain:[0,36000]
+    },
+  })
+  d3.select('#chart2').append(() => chart)
+})
--- dataviz_3/script.js
+++ dataviz_3/script.js
@@ -1,19 +1,28 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-    let chart = Plot.plot({
-      marks: [
-        Plot.barX(data, {
-          x: 'mision_hs',
-          y: 'ocupacion',
-        }),
-      ],
-      y: {
-        domain: d3.sort(data, (a, b) => d3.descending(a.mision_hs, b.mision_hs)).map(d => d.ocupacion),
-      },
-      x: {
-        grid: true,
-      },
-      height: 500,
-      marginLeft: 200,
-    })
-    d3.select('#chart').append(() => chart)
+  let chart = Plot.plot({
+    marks: [
+      Plot.barX(
+        data,
+        Plot.groupY({ x: "sum" }, { x: "mision_hs", y: "nacionalidad", sort: {y:"x", reverse:true}, })
+      )
+    ],
+    style: {
+      backgroundColor: "#7e9a9a",
+      color: "#e5fffa",
+      fontFamily: "system-ui",
+      fontSize: "14px",
+      overflow: "visible"
+    },
+    margin:50, 
+    height: 500,
+    widht: 500,
+    marginLeft: 200,
+    y: {
+      
+    },
+    x: {
+      grid: true,
+    }
   })
+  d3.select('#chart').append(() => chart)
+})
